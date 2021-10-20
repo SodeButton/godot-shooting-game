792fbd6b469a72c05743a6a37b81eba2e7635f5d
SodeButton <takuya.20020824@gmail.com>
2021-10-20 22:36:27 +0900

    update player

--- Player.gd
+++ Player.gd
@@ -4,7 +4,21 @@ export var speed = 400
 var screen_size
 
 func _ready():
-	pass # Replace with function body.
-
+	screen_size = get_viewport_rect().size
+	
 func _process(delta):
-	pass
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 16, screen_size.x - 16)
+	position.y = clamp(position.y, 16, screen_size.y - 16)
